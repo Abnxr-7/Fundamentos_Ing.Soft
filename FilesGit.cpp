e4351bbe1a146ab28cca1f0b11d31aa5c9caee8e
Abnxr-7 <abnerfab0@gmail.com>
2025-02-04 09:43:33 -0500

    fist file

---
+++ FilesGit.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_ELEMENTOS 3
+#define TAMANO_NOMBRE 25
+
+//define la variable tipo typedef
+typedef struct{
+int clave;
+char usuario(TAMANO_NOMBRE)
+}
+
+//definir varibles
+Persona;
+typedef Persona TipoELemento;
+typedef int posicion;
+
+typedef
+{
+    TipoElemento elementos [MAX_ELEMENTOS];
+    posicion ult;
+}
+
+Lista;
+//decalracion de variables
+int Posicion;
+Persona Alumno1;
+Persona Alumno2;
+Persona Alumno3;
+
+Lista MiLista;
+//especificamos los valores de usuario y clave
+
+void inicializador()
+{
+    strcpy(Alumno1.usuario,"Luis");
+    Alumno1.calve = 100;
+    strcpy(Alumno1.usuario,"Juan");
+    Alumno.clave = 200;
+    strcpy(Alumno3.usuario,"Pedro");
+    Alumno.calve = 300;
+
+    Milista.elementos [0] = Allumno1;
+    Milista.elementos [1] = Allumno2;
+    Milista.elementos [3] = Allumno3;
+}
+//reallizamos la implementacion
